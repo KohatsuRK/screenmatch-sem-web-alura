d154e523f7954cfe82af1b140775cdbf3edd4ffd
Renan Kohatsu <rkohatsu@unifesp.br>
2024-06-29 13:18:54 -0300

    Api chatGPT descontinuado, Integração com MemoryAPI

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -4,17 +4,17 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
-    public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("key aqui");
-
-        CompletionRequest requisicao = CompletionRequest.builder()
-                .model("gpt-3.5-turbo")
-                .prompt("traduza para o português o texto: " + texto)
-                .maxTokens(1000)
-                .temperature(0.7)
-                .build();
-
-        var resposta = service.createCompletion(requisicao);
-        return resposta.getChoices().get(0).getText();
-    }
+//    public static String obterTraducao(String texto) {
+//        OpenAiService service = new OpenAiService("key aqui");
+//
+//        CompletionRequest requisicao = CompletionRequest.builder()
+//                .model("gpt-3.5-turbo")
+//                .prompt("traduza para o português o texto: " + texto)
+//                .maxTokens(1000)
+//                .temperature(0.7)
+//                .build();
+//
+//        var resposta = service.createCompletion(requisicao);
+//        return resposta.getChoices().get(0).getText();
+//    }
 }
