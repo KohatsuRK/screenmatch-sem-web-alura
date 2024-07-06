2669307ddc40df7d01a1d5d34c247c6c59f935e5
Renan Kohatsu <rkohatsu@unifesp.br>
2024-07-06 19:30:40 -0300

    gravando episodios em banco de dados

--- src/main/java/br/com/alura/screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -1,16 +1,29 @@
 package br.com.alura.screenmatch.model;
 
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
+@Table(name = "episodios")
 public class Episodio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Integer temporada;
     private String titulo;
     private Integer numeroEpsodio;
     private Double avaliacao;
     private LocalDate dataDeLancamento;
 
+
+    @ManyToOne
+    private Serie serie;
+
+    public Episodio() {}
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -27,6 +40,23 @@ public class Episodio {
             this.dataDeLancamento = null;
         }
 
+    }
+
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Integer getTemporada() {
